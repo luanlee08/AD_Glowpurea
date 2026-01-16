f6d6d331446dd9521f7cba36f777be511e738cfc
luanlee08 <luanlevn2@gmail.com>
2026-01-16 13:17:13 +0700

    fix lỗi đăng nhập

--- src/lib/axiosAdmin.ts
+++ src/lib/axiosAdmin.ts
@@ -1,3 +1,4 @@
+// lib/axiosAdmin.ts
 import axios from "axios";
 
 const axiosAdmin = axios.create({
@@ -5,13 +6,33 @@ const axiosAdmin = axios.create({
 });
 
 axiosAdmin.interceptors.request.use((config) => {
-  const token = localStorage.getItem("admin_token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("admin_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
-
   return config;
 });
+
+axiosAdmin.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const status = error?.response?.status;
+
+    if (status === 401 || status === 403) {
+      // 🔥 BỊ ĐÁ SESSION
+      localStorage.removeItem("admin_token");
+      localStorage.removeItem("admin_email");
+
+      // redirect về login
+      if (typeof window !== "undefined") {
+        window.location.href = "/signin";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosAdmin;
